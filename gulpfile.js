574eeca25932e793b43cb7d308af5d606d2580b5
宁尘 <guanghui.ygh@alibaba-inc.com>
2018-10-12 02:42:56 +0800

    feat: 修改任务

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 
 const gulp = require('gulp');
 
-// gulp.task('build', () => {
-//   gulp.src('./index.js')
-//     .pipe(gulp.dest('./lib/'));
-// });
+gulp.task('build', () => {
+  gulp.src('./index.js')
+    .pipe(gulp.dest('./lib/'));
+});
 
 gulp.task('bin', () => {
   gulp.src('./index.js')
