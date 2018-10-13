216f401a113048b177f784206b6cdaf56c3b40f5
nc-theone <telljsn@sina.cn>
2018-10-14 01:51:46 +0800

    test: test

--- commands/index.js
+++ commands/index.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: 宁尘
+ * @Date: 2018-10-14 01:51:01
+ * @Last Modified by:   宁尘
+ * @Last Modified time: 2018-10-14 01:51:01
+ */
 
 const switchCmd = require('./switch.js');
 const openCmd = require('./open.js');
